86c44c5280dec00720a7ab487c19059fb76f5fdf
athena830 <athena830@gmail.com>
2017-08-10 11:17:00 +0800

    Modify fileName, functionName, production code refactor

--- PrimeString/PrimeString/Kata.cs
+++ PrimeString/PrimeString/Kata.cs
@@ -1,35 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
 namespace PrimeString
 {
     public class Kata
     {
         public bool PrimeString(string source)
         {
-            //如果是單數，就是prime string (X)
-            //如果只有一個字，就是prime string
-            if (source.Length == 1)
-            {
-                return true;
-            }
+            if (OnlyOneWord_Should_Be_True(source)) return true;
 
-            //把整體切一半，如果有重覆就不是prime string (X)
-            //每次把整體切一半，然後把重覆的取代掉 (X)
-            //先取第一個字，然後取到重覆第一個字的位置並取代掉 (X)
-            //整體切一半，然後組出一個重覆的pattern，並取代掉原本的source
-            for (int i = 0; i < source.Length/2; i++)
+            for (var i = 0; i < source.Length / 2; i++)
             {
-                var pattern = source.Substring(0, i + 1);
-                if (source.Replace(pattern, "") == "")
+                if (!Source_Be_Replace_All_Should_Be_PrimeString(source, i))
                 {
                     return false;
                 }
             }
 
             return true;
+        }
+
+        private static bool Source_Be_Replace_All_Should_Be_PrimeString(string source, int position)
+        {
+            return source.Replace(source.Substring(0, position + 1), "") != "";
+        }
+
+        private static bool OnlyOneWord_Should_Be_True(string source)
+        {
+            return source.Length == 1;
         }
     }
 }
---
+++ PrimeString/PrimeString/PrimeStringTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NUnit.Framework;
+
+namespace PrimeString
+{
+    [TestFixture]
+    public class PrimeStringTest
+    {
+        [Test]
+        public void Input_a_Should_Be_True()
+        {
+            Assert_Should_Be_PrimeString("a");
+        }
+
+        [Test]
+        public void Input_x_Should_Be_True()
+        {
+            Assert_Should_Be_PrimeString("x");
+        }
+
+
+        [Test]
+        public void Input_abc_Should_Be_True()
+        {
+            Assert_Should_Be_PrimeString("abc");
+        }
+
+        [Test]
+        public void Input_abac_Should_Be_True()
+        {
+            Assert_Should_Be_PrimeString("abac");
+        }
+
+        [Test]
+        public void Input_abba_Should_Be_True()
+        {
+            Assert_Should_Be_PrimeString("abba");
+        }
+
+        [Test]
+        public void Input_abab_Should_Be_False()
+        {
+            Assert_Should_Not_Be_PrimeString("abab");
+        }
+
+        [Test]
+        public void Input_aaaa_Should_Be_False()
+        {
+            Assert_Should_Not_Be_PrimeString("aaaa");
+        }
+
+        [Test]
+        public void Input_fdsyffdsyffdsyffdsyffdsyf_Should_Be_False()
+        {
+            Assert_Should_Not_Be_PrimeString("fdsyffdsyffdsyffdsyffdsyf");
+        }
+
+        [Test]
+        public void Input_utdutdtdutd_Should_Be_True()
+        {
+            Assert_Should_Be_PrimeString("utdutdtdutd");
+        }
+
+        private static void Assert_Should_Be_PrimeString(string actual)
+        {
+            Assert.AreEqual(true, new Kata().PrimeString(actual));
+        }
+
+        private static void Assert_Should_Not_Be_PrimeString(string actual)
+        {
+            Assert.AreEqual(false, new Kata().PrimeString(actual));
+        }
+    }
+}
